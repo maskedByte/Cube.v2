90e7e8e5181bbf0f427c759a407f205f053624c9
Tony <Tony.Haase@quibiq.de>
2023-08-16 22:47:13 +0200

    Improved rect mesh generation and rename to quad

--- 02_Service/Engine.Framework/Rendering/Shapes/QuadMesh.cs
+++ 02_Service/Engine.Framework/Rendering/Shapes/QuadMesh.cs
@@ -5,26 +5,26 @@ using Engine.Core.Math.Vectors;
 
 namespace Engine.Framework.Rendering.Shapes;
 
-public class RectMesh : Mesh
+public sealed class QuadMesh : Mesh
 {
-    public RectMesh(IContext context)
+    public QuadMesh(IContext context)
         : base(context)
 
     {
         Vertices = new[]
         {
-            new Vertex(new Vector3(0f, 0f, 0f), Color.White),
-            new Vertex(new Vector3(1f, 0f, 0f), Color.White),
-            new Vertex(new Vector3(1f, 1f, 0f), Color.White),
-            new Vertex(new Vector3(0f, 1f, 0f), Color.White)
+            new Vertex(new Vector3(-.5f, -.5f, 0f), Color.White),
+            new Vertex(new Vector3(.5f, -.5f, 0f), Color.White),
+            new Vertex(new Vector3(.5f, .5f, 0f), Color.White),
+            new Vertex(new Vector3(-.5f, .5f, 0f), Color.White)
         };
 
         UvCoordinates = new[]
         {
-            new Vector2(0f, 1f),
-            new Vector2(1f, 1f),
+            new Vector2(0f, 0f),
             new Vector2(1f, 0f),
-            new Vector2(0f, 0f)
+            new Vector2(1f, 1f),
+            new Vector2(0f, 1f)
         };
 
         Indices = new[]
