dc9ff1f4897c0324e76af18376b13cf38423bbcb
Tony <Tony.Haase@quibiq.de>
2023-08-17 23:21:25 +0200

    Make the RenderElementCommand the highest priority

--- 08_Domain/Engine.Rendering/Commands/RenderCommands/RenderElementCommand.cs
+++ 08_Domain/Engine.Rendering/Commands/RenderCommands/RenderElementCommand.cs
@@ -1,9 +1,13 @@
 ﻿namespace Engine.Rendering.Commands.RenderCommands;
 
+/// <summary>
+///     Render an element has high priority to ensure that it always handled last.
+/// </summary>
 public sealed class RenderElementCommand : CommandBase
 {
     public RenderElementCommand()
         : base(CommandType.RenderElement)
     {
+        Priority = 999999999;
     }
 }
