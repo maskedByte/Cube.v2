c3e56e569ee05bf3b2c1646f1e4eebe9ee2e74a6
Tony <Tony.Haase@quibiq.de>
2024-01-26 13:58:47 +0100

    Add SpriteRenderingSystem

---
+++ 02_Service/Engine.Framework/Systems/SpriteRenderingSystem.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Core.Driver;
+using Engine.Framework.Components;
+using Engine.Framework.Entities;
+using Engine.Rendering.Commands;
+
+namespace Engine.Framework.Systems;
+
+public class SpriteRenderingSystem : SystemBase<SpriteRendererComponent>
+{
+    public SpriteRenderingSystem(IContext context)
+        : base(context)
+    {
+    }
+
+    public override void Handle(SystemStage stage, IComponent component, ICommandQueue commandQueue, float deltaTime)
+    {
+        var materialComponent = (MaterialComponent)component;
+        var material = materialComponent.Material;
+    }
+}
