3e454a016c445b8e1910540203b86921a3818537
Tony <Tony.Haase@quibiq.de>
2024-01-26 13:31:41 +0100

    Add SpriteRenderer

---
+++ 02_Service/Engine.Framework/Components/SpriteRendererComponent.cs
@@ -0,0 +1,10 @@
+﻿using Engine.Framework.Entities;
+using Engine.Rendering.Shapes;
+
+namespace Engine.Framework.Components;
+
+public sealed class SpriteRendererComponent : IComponent
+{
+    public IEntity Owner { get; set; } = null!;
+    public IMesh Mesh { get; set; } = null!;
+}
